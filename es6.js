81694f3b50b6907ff68391760e22b573c1943e27
Imran <imurokks@gmail.com>
2020-05-02 19:35:52 +0530

    first commit

---
+++ es6.js
@@ -0,0 +1,28 @@
+let greet = "hello";
+console.log(greet);
+
+
+// blocks
+
+// arrow functions
+
+// strings (includes, repeat)
+
+// template strings
+
+// destructuring  arrays / objs
+
+// module exports , imports
+
+// default parameters
+
+// rest parameters
+
+// named parameters
+
+// spread operators
+
+// classes
+
+// maps - get, set, has
+
